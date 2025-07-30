6dd85ce2e8bb4c62f23f5f628c8810fe95eabc55
TechieZed <jasz56789@gmail.com>
2025-07-30 12:15:03 -0400

    add docstrings to functions

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from stats import number_of_words
 
 def get_book_text(file_path):
+    """Get contents of book file in .txt format.
+
+    :param file_path: str - relative path to .txt book file
+    :return: str - full text of the book file
+
+    Function that takes the file path to the .txt book file, reads the file, and then returns its full contents.
+    """
+    
     with open(file_path) as f:
         return f.read()
 
 def main():
+    
     num_words = number_of_words(get_book_text("books/frankenstein.txt"))
+
     print(f"{num_words} words found in the document")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,21 @@
 def number_of_words(book_text):
-    numbers = len(book_text.split())
-    return numbers
+    """Get number of words in a book file.
+
+    :param book_text: str - full text of book
+    :return: int - number of total words in the book
+
+    Function that takes the full string of text extracted from a book file, and returns the total number of words in the book file as an integer.
+    """
+
+    return len(book_text.split())
+
+def number_of_chars(book_text):
+    """Count the number of characters in a book.
+
+    :param book_text: str - full text of book
+    :return: dict - dictionary with characters as the key and number of occurences as the value
+
+    Function that takes the full text of a book and returns a dictionary containing each character and the number of times each character occurs in the book.
+    """
+
+    pass
